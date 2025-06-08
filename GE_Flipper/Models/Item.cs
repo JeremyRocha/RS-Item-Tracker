82ab4e944de6184628fda1dd3fd3b7cfc094c6dc
Jeremy Rocha <jeremyrocha@rogers.com>
2025-06-08 13:34:20 -0400

    Added Item and ItemCategory models with one-to-many relationship

---
+++ GE_Flipper/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GE_Flipper.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }  // Primary Key
+
+        [Required]
+        public string Name { get; set; }
+
+        public int GEId { get; set; }  // Grand Exchange ID
+
+        public decimal LatestPrice { get; set; }
+
+        // Foreign key to ItemCategory
+        [ForeignKey("ItemCategory")]
+        public int ItemCategoryId { get; set; }
+
+        public ItemCategory ItemCategory { get; set; }
+    }
+}
---
+++ GE_Flipper/Models/ItemCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GE_Flipper.Models;
+
+public class ItemCategory
+{
+    public int ItemCategoryId { get; set; }  // Primary Key
+
+    [Required]
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string IconUrl { get; set; }
+
+    public ICollection<Item> Items { get; set; }
+}
