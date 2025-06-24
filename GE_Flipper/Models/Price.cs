286460855f32c939621ab449cd356637efc6012c
Jeremy Rocha <jeremyrocha@rogers.com>
2025-06-24 15:57:44 -0400

    Set up price model

---
+++ GE_Flipper/Models/Price.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GE_Flipper.Models
+{
+    public class Price
+    {
+        [Required]
+        public int PriceID { get; set; }
+        [Required]
+        public int currentPrice {  get; set; }
+        [Required]
+        public DateTime date { get; set; }
+
+        [ForeignKey("Item")]
+        public int ItemId { get; set; }
+
+        [ValidateNever]
+        public Item Item{ get; set; } = null!;
+    }
+}
