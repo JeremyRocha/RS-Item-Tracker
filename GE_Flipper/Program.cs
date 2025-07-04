19e85280e0c0f756232b96d233bf803e43609ec7
Jeremy Rocha <jeremyrocha@rogers.com>
2025-07-03 23:29:42 -0400

    added registration and updated db connection settings

--- GE_Flipper/Program.cs
+++ GE_Flipper/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddHttpClient(); //Adds httpClient to build
 builder.Services.AddHostedService<PriceGetter>(); //Adds background task to the build
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
